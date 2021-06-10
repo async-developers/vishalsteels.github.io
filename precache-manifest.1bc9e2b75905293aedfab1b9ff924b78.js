4cf97c9f4ae1b3b67d8efe7bce5582ad3c8d3203
mohit1521 <77525015+async-developers@users.noreply.github.com>
2021-06-10 22:59:17 +0530

    Webapp

---
+++ precache-manifest.1bc9e2b75905293aedfab1b9ff924b78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05517dce0ee5ce7c364145ff35987b49",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "de31dea0df0e566a066d",
+    "url": "/react-invoice-generator/static/css/2.9fca9f18.chunk.css"
+  },
+  {
+    "revision": "9225da7504a607e25398",
+    "url": "/react-invoice-generator/static/css/main.373062f0.chunk.css"
+  },
+  {
+    "revision": "de31dea0df0e566a066d",
+    "url": "/react-invoice-generator/static/js/2.49dafd7b.chunk.js"
+  },
+  {
+    "revision": "8c65b9cc60f7a3841035f7df21a421d4",
+    "url": "/react-invoice-generator/static/js/2.49dafd7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9225da7504a607e25398",
+    "url": "/react-invoice-generator/static/js/main.164e6ab6.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "273d8cd01d6b768efed9f7c06df1577e",
+    "url": "/react-invoice-generator/static/media/footer-image.273d8cd0.jpg"
+  }
+]);
